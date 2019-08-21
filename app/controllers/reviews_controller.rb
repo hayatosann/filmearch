02a73388f1ab31895099fe6597a31a62336c7d4a
hayatosann <sasano.hayato@icloud.com>
2019-08-21 14:52:47 +0900

    set destroy action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,9 +8,25 @@ class ReviewsController < ApplicationController
 
  def create
   Review.create(create_params)
-  # binding.pry
   redirect_to controller: :products, action: :index
  end
+
+ def destroy
+  review = Review.find(params[:id])
+  @review=review.destroy if review.user.nickname == current_user.nickname
+  redirect_to controller: :products, action: :index
+ end
+
+#  def edit
+#   @review = Review.find(params[:id])
+#  end
+
+#  def update
+#   review = Review.find(params[:id])
+#   if review.user.nickname == current_user.nickname
+#     review.update(create_params)
+#   end
+# end
 
  private
   def create_params
