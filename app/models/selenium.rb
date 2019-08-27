91fe1f9de3c4eda1d0b8a469a6aa9e027600eb17
hayatosann <sasano.hayato@icloud.com>
2019-08-27 13:01:53 +0900

    set commentout

--- app/models/selenium.rb
+++ app/models/selenium.rb
@@ -1,13 +1,13 @@
-require 'selenium-webdriver'
-driver = Selenium::WebDriver.for :chrome
-wait = Selenium::WebDriver::Wait.new(timeout: 80)
-driver.navigate.to 'https://filmarks.com/list/genre/8'
+# require 'selenium-webdriver'
+# driver = Selenium::WebDriver.for :chrome
+# wait = Selenium::WebDriver::Wait.new(timeout: 80)
+# driver.navigate.to 'https://filmarks.com/list/genre/8'
   
-# driver.find_elements(:xpath, '//img')
-element=driver.find_element(:xpath, '/html/body/div[4]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]/div[6]/a')
-  detail = driver.find_element(:xpath, '//*[@id="js-content-detail-synopsis"]/div/div/button').click
-  more_detail = driver.find_element(:id, 'js-content-detail-synopsis').text
- puts more_detail
+# # driver.find_elements(:xpath, '//img')
+# element=driver.find_element(:xpath, '/html/body/div[4]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]/div[6]/a')
+#   detail = driver.find_element(:xpath, '//*[@id="js-content-detail-synopsis"]/div/div/button').click
+#   more_detail = driver.find_element(:id, 'js-content-detail-synopsis').text
+#  puts more_detail
 
 
 # detail = driver.find_elements(:class,'p-content-detail__synopsis-desc is-show-all')
