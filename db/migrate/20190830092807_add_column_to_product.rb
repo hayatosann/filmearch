fe1436eb0ee01f389bd30b180fe22bf19fb71ef7
hayatosann <sasano.hayato@icloud.com>
2019-08-31 14:04:41 +0900

    remove column

--- db/migrate/20190830092807_add_column_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToProduct < ActiveRecord::Migration[5.2]
-  def change
-    add_column :products, :genre, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_092807) do
+ActiveRecord::Schema.define(version: 2019_08_01_115605) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2019_08_30_092807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes_count"
-    t.string "genre"
   end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
